b6f1a41ebb601d18f73f35014b86854bba28a196
pgoyal <pgoyal@paymentus.com>
2024-09-06 17:08:58 +0530

    9/6/2024 05:08

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "872992685"
+    serviceWorkerVersion: "1262492955"
   }
 });
