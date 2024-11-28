d54b68b842edecaf85f9c4f14959b3ade00ad2d1
pgoyal <pgoyal@paymentus.com>
2024-11-28 14:13:43 +0530

    28/11/2024 2:13PM

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -9,12 +9,8 @@ if (!window._flutter) {
 _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e31a","builds":[{"compileTarget":"dart2js","renderer":"html","mainJsPath":"main.dart.js"}]};
 
 
-// Modify only the mainJsPath by adding query string
-_flutter.buildConfig.builds[0].mainJsPath = 'main.dart.js?v=' + "1961150950";
-
-//Initialize configuration settings and start the application boot process
 _flutter.loader.load({
-    serviceWorkerSettings: {
-        serviceWorkerVersion: "1961150950",
-   }
+  serviceWorkerSettings: {
+    serviceWorkerVersion: "683093689"
+  }
 });
